76c8ed759005242b74353dcd20b0017141085d52
sorkcoavus <sorkcoavus@gmail.com>
2023-01-10 23:46:47 +0900

    lombok

--- Ch07/src/main/java/kr/co/ch07/vo/User1VO.java
+++ Ch07/src/main/java/kr/co/ch07/vo/User1VO.java
@@ -1,34 +1,22 @@
 package kr.co.ch07.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User1VO {
 	private String uid;
 	private String name;
 	private String hp;
 	private int age;
 	
-	public String getUid() {
-		return uid;
-	}
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getHp() {
-		return hp;
-	}
-	public void setHp(String hp) {
-		this.hp = hp;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
+	
 	
 }
