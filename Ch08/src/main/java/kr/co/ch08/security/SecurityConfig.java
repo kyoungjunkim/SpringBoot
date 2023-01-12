4ea6232af2a9a5df2205bea272ebc56abc719154
sorkcoavus <sorkcoavus@gmail.com>
2023-01-12 13:43:09 +0900

    23/1/12

--- Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
+++ Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import kr.co.ch08.service.User2Service;
@@ -54,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser("member").password("{noop}1234").roles("MEMBER");
 		
 		//로그인 인증 처리서비스, 암호화 방식 설정
-		//auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(userService).passwordEncoder(new MessageDigestPasswordEncoder("SHA-256"));
 	
 	
 	}
