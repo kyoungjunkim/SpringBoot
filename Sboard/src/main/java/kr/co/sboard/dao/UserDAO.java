9385a46ac2d78141b61a5d59e71fcd89f1143bd1
sorkcoavus <sorkcoavus@gmail.com>
2023-01-18 11:18:21 +0900

    23/1/18

---
+++ Sboard/src/main/java/kr/co/sboard/dao/UserDAO.java
@@ -0,0 +1,20 @@
+package kr.co.sboard.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import kr.co.sboard.entity.UserEntity;
+import kr.co.sboard.vo.TermsVO;
+
+@Mapper
+@Repository
+public interface UserDAO {
+
+	public int insertUser(UserEntity vo);
+	public TermsVO selectTerms();
+	public void selectUser();
+	public void selectUsers();
+	public void updateUser();
+	public void deleteUser();
+	
+}
