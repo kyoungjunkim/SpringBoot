00dd45592f1d24af5e208d4c6b42e7c8e93c1be6
sorkcoavus <sorkcoavus@gmail.com>
2023-01-20 13:30:16 +0900

    23/1/20

--- Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
+++ Sboard/src/main/java/kr/co/sboard/controller/ArticleController.java
@@ -1,11 +1,14 @@
 package kr.co.sboard.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.co.sboard.service.ArticleService;
 import kr.co.sboard.vo.ArticleVO;
+import kr.co.sboard.vo.FileVO;
 
 @Controller
 public class ArticleController {
@@ -21,10 +25,23 @@ public class ArticleController {
 	private ArticleService service;
 	
 	@GetMapping("list")
-	public String list(Principal principal, Model model) {
+	public String list(Model model, String pg) {
 		
-		List<ArticleVO> articles = service.selectArticles();
+		int currentPage = service.getCurrentPage(pg);
+		int start = service.getLimitStart(currentPage);
+		
+		int total = service.selectCountTotal();
+		int lastPageNum = service.getLastPageNum(total);
+		int pageStartNum = service.getPageStartNum(total, start);
+		int groups[] = service.getPageGroup(currentPage, lastPageNum);
+		
+		List<ArticleVO> articles = service.selectArticles(start);
+		
 		model.addAttribute("articles", articles);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("lastPageNum", lastPageNum);
+		model.addAttribute("pageStartNum", pageStartNum);
+		model.addAttribute("groups", groups);
 		
 		return "list";
 	}
@@ -35,9 +52,24 @@ public class ArticleController {
 	}
 	
 	@GetMapping("view")
-	public String view() {
+	public String view(int no, Model model) {
+		ArticleVO article = service.selectArticle(no);
+		model.addAttribute("article", article);
 		return "view";
 	}
+	
+	@GetMapping("download")
+	public ResponseEntity<Resource> download(int fno) throws IOException{
+		//파일조회
+		FileVO vo = service.selectFile(fno);
+		//파일 다운로드 카운터 증가
+		service.updateFileDownload(fno);
+		//파일 다운로드
+		ResponseEntity<Resource> respEntity = service.fileDownload(vo);
+		
+		return respEntity;
+	}
+	
 	
 	@GetMapping("write")
 	public String write() {
@@ -46,11 +78,12 @@ public class ArticleController {
 	
 	@PostMapping("write")
 	public String write(ArticleVO vo, HttpServletRequest req) {
-		
 		String regip = req.getRemoteAddr();
 		vo.setRegip(regip);
 		
+				
 		service.insertArticle(vo);
+		
 		return "redirect:/list";
 	}
 	
