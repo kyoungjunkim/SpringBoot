4f98b4fe94db93e978c009818d9c42bd28467897
sorkcoavus <sorkcoavus@gmail.com>
2023-04-25 11:35:11 +0900

    2023/04/25 Voard

---
+++ Voard/src/main/java/kr/co/voard/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package kr.co.voard.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import kr.co.voard.security.MyUserDetails;
+import kr.co.voard.security.SecurityUserService;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private SecurityUserService securityUserService;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		log.info("doFilterInternal...1");
+		
+		String token = jwtUtil.resolveToken(request);
+		log.info("doFilterInternal...2 : " + token);
+		
+		if(token != null && jwtUtil.validateToken(token)) {
+			log.info("doFilterInternal...3");
+			String uid = jwtUtil.getUsernameFromToken(token);
+			
+			MyUserDetails myUserDetails = securityUserService.loadUserByUsername(uid);
+			
+			// Security 인증처리
+			UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(myUserDetails, 
+																										 null, 
+																										 myUserDetails.getAuthorities());
+			
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			
+		}
+		
+		
+		// 다음 필터로 이동
+		filterChain.doFilter(request, response);
+	}
+
+}
---
+++ Voard/src/main/java/kr/co/voard/security/SecurityUserService.java
@@ -0,0 +1,35 @@
+package kr.co.voard.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import kr.co.voard.repository.UserEntity;
+import kr.co.voard.repository.UserRepo;
+
+
+@Service
+public class SecurityUserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepo repo;
+	
+	@Override
+	public MyUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		UserEntity user = repo.findById(username).get();
+		
+		if(user == null) {
+			throw new UsernameNotFoundException(username); 
+		}
+		
+		MyUserDetails myUserDetails = MyUserDetails.builder()
+								.user(user)
+								.build();
+		
+		return myUserDetails;
+	}
+
+}
